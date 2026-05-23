08d9f158ae93a8aad8737d336262237597751bc6
sayan <sayan06.dev@gmail.com>
2026-05-23 14:19:54 +0530

    feat: add Inngest function to automate code generation and PR creation for issues

--- app/api/inngest/route.js
+++ app/api/inngest/route.js
@@ -1,14 +1,16 @@
 import { serve } from "inngest/next";
 import { inngest } from "../../../lib/inngest/client";
 import { agentFunction } from "../../../lib/inngest/functions";
+import { codeAgentFunction } from "../../../lib/inngest/codeAgentFunction";
 
 export const runtime = "nodejs";
 
 const handler = serve({
   client: inngest,
-  functions: [agentFunction],
+  functions: [agentFunction, codeAgentFunction],
 });
 
 export const GET = handler;
 export const POST = handler;
 export const PUT = handler;
+
